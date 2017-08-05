90dfda20e9cebacd403e64f959116152f4bf6ea5
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-08-04 22:23:21 -0700

    update the hw for w2d1

--- W1D5/Poly Tree Node/lib/00_tree_node.rb
+++ W1D5/Poly Tree Node/lib/00_tree_node.rb
@@ -1,3 +1,16 @@
-class PolyTreeNode
+require 'byebug'
 
+class PolyTreeNode
+  attr_accessor :value, :parent, :children
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(node)
+    # byebug
+    @parent = node
+    node.children += @children
+  end
 end
