cc8019edf3f3ba0775cd16ce2099c7d02645a6bb
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-08-06 14:40:06 -0700

    Finish Tic Tac Toe Node class

---
+++ W1D5/Tic Tac Toe AI/lib/tic_tac_toe_node.rb
@@ -0,0 +1,62 @@
+require_relative 'tic_tac_toe'
+require 'byebug'
+
+class TicTacToeNode
+  attr_accessor :board,  :next_mover_mark, :prev_move_pos
+  # attr_reader :next_mover_mark, :prev_move_pos
+
+  def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
+  end
+
+  def losing_node?(evaluator)
+    if @board.over?
+      # make sure there is no tie, the current player is not winner
+      @board.won? && @board.winner != evaluator
+    elsif @next_mover_mark == evaluator
+      # current player turn
+      # detect every child is a loser
+      self.children.all? { |child| child.losing_node?(evaluator) }
+    else
+      # opponent's turn
+      # detect any child is a loser
+      self.children.any? { |child| child.losing_node?(evaluator)  }
+    end
+  end
+
+  def winning_node?(evaluator)
+    if @board.won?
+      # the game has a winner and current player is the winner
+      @board.winner == evaluator
+    elsif @next_mover_mark == evaluator
+      # current player's turn, true if any child of current is winner
+      self.children.any? { |child| child.winning_node?(evaluator) }
+    else
+      # opponent's turn, true if all children of curren is winner
+      self.children.all? { |child| child.winning_node?(evaluator) }
+    end
+  end
+
+  # This method generates an array of all moves that can be made after
+  # the current move.
+  def children
+    arr = []; length = @board.rows.length
+
+    (0...length).each do |x|
+      (0...length).each do |y|
+        pos = [x, y]
+        if @board[pos].nil?
+          next_mark = (@next_mover_mark == :x) ? :o : :x
+          new_board = @board.dup
+          new_board[pos] = next_mark
+          kid = TicTacToeNode.new(new_board, next_mark, pos)
+          arr << kid
+        end
+      end
+    end
+
+    arr
+  end
+end
