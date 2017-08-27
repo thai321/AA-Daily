c6c963faa6875cb846f951ed661ad414297ad29f
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-08-26 18:36:51 -0700

    Add rails-testing practice

---
+++ Practices/Rails/rails-testing/config/application.rb
@@ -0,0 +1,27 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module RailsTesting
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => false
+
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
+
+  end
+end
---
+++ Practices/Rails/rails-testing/spec/factories/test_models.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :test_model do
+    
+  end
+end
