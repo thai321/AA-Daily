93da602962ed6cd2e0b49d03cc1df1027668a674
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-08-27 11:31:32 -0700

    Update and finish W5D1 homework

--- homeworks/W5D1/MusicApp/app/controllers/users_controller.rb
+++ homeworks/W5D1/MusicApp/app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
       redirect_to bands_url
     else
       flash.now[:errors] = @user.errors.full_messages
-      render :new, status: 400
+      render :new
     end
   end
 
--- homeworks/W5D1/MusicApp/spec/features/auth_spec.rb
+++ homeworks/W5D1/MusicApp/spec/features/auth_spec.rb
@@ -8,23 +8,16 @@ feature "the signup process" do
   end
 
   feature "signing up a user" do
-    # before(:each) do
-    #   # visit new_user_url
-    #   visit '/users/new/'
-    #   fill_in 'Email', :with => "testing@email.com"
-    #   fill_in 'password', :with => "biscuits"
-    #   click_on "Create User"
-    # end
-
-    scenario "redirects to bands index page after signup" do
-      # byebug
-      # puts "SDFSDFSDFSDFSD"
-      # expect(current_path).to eq bands_path
+    before(:each) do
       visit new_user_url
       fill_in 'Email', :with => "testing@email.com"
       fill_in 'password', :with => "biscuits"
-      click_on "create user"
-      expect(page).to have_content("hello")
+      click_on "Create User"
+    end
+
+    scenario "redirects to bands index page after signup" do
+      expect(current_path).to eq bands_path
+      expect(page).to have_content "List of bands"
     end
   end
 
@@ -32,10 +25,12 @@ feature "the signup process" do
     before(:each) do
       visit new_user_url
       fill_in 'Email', :with => "testing@email.com"
-      click_on "create user"
+      click_on "Create User"
     end
 
-    scenario "re-renders the signup page after failed signup"
+    scenario "re-renders the signup page after failed signup" do
+      expect(page).to have_content "Password is too short (minimum is 6 characters)"
+    end
   end
 
 end
