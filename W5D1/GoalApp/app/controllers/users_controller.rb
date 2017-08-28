cf52a5f065e9a19cf202b499e4da589babe5a16c
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-08-27 18:01:27 -0700

    Finish users controller test

--- W5D1/GoalApp/app/controllers/users_controller.rb
+++ W5D1/GoalApp/app/controllers/users_controller.rb
@@ -1,21 +1,32 @@
 class UsersController < ApplicationController
   def index
+    @user = User.all
+    render :index
   end
 
   def new
+    @user = User.new
+    render :new
   end
 
   def create
-  end
-
-  def destroy
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
   end
 
   def show
-    
+    @user = User.find(params[:id])
+    render :show
   end
 
-  def update
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 
 end
--- W5D1/GoalApp/spec/controllers/users_controller_spec.rb
+++ W5D1/GoalApp/spec/controllers/users_controller_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-    subject
+    subject(:thai) { FactoryGirl.build(:thai) }
 
 
   describe "GET #index" do
-    it "returns http success" do
+    it "returns the index page and http success" do
       get :index
       expect(response).to render_template('index')
       expect(response).to have_http_status(:success)
@@ -14,7 +14,7 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe "GET #new" do
-    it "returns http success" do
+    it "returns the new page http success" do
       get :new
       expect(response).to render_template('new')
       expect(response).to have_http_status(:success)
@@ -22,34 +22,73 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe "POST #create" do
-    xit "returns http success" do
-      get :create
+    context "with invalid params" do
+      it "validate username's uniqueness" do
+        thai.save
+        post :create, params: { user: {
+            username: 'thai',
+            password: 'password'
+          }
+        }
+        expect(response).to render_template("new")
+        expect(flash[:errors]).to be_present
+      end
 
-      expect(response).to have_http_status(:success)
+      it "validate the username's present" do
+        post :create, params: { user: {
+          username: '',
+          password: 'password'
+          }
+        }
+
+        expect(response).to render_template("new")
+        expect(flash[:errors]).to be_present
+      end
+
+      it "validates the password's present" do
+        post :create, params: { user: {
+          username: 'thai',
+          password: ''
+          }
+        }
+
+        expect(response).to render_template("new")
+        expect(flash[:errors]).to be_present
+      end
+
+
+      it "validates the password's length is at least 6 characters" do
+        post :create, params: { user: {
+          username: 'thai',
+          password: '12345'
+          }
+        }
+
+        expect(response).to render_template("new")
+        expect(flash[:errors]).to be_present
+      end
+    end
+
+    context "with valid params" do
+      it "redirect to user show page with success" do
+        post :create, params: { user: {
+            username: 'thai1',
+            password: 'password'
+          }
+        }
+        expect(response).to redirect_to(User.last)
+      end
     end
   end
 
-  describe "DELETE #destroy" do
-    xit "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      thai.save
+      get :show, params: { id: thai.id }
       expect(response).to render_template('show')
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "GET #update" do
-    xit "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 
 end
