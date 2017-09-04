d833d4c979f7dded551da5b074e1d614048f729e
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-09-04 10:43:59 -0700

    Add W6D1 initial state commit

---
+++ W6D1/arguments_exercises/arguments_exercise.js
@@ -0,0 +1,5 @@
+// Write a sum function that takes any number of arguments:
+//  sum(1, 2, 3, 4) === 10
+// sum(1, 2, 3, 4, 5) === 15
+// Solve it first using the arguments keyword,
+// then rewrite your solution to use the ... rest operator.
