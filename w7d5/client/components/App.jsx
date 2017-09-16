ec89e3c7076c09697d07dcd1c633e30b55cc928e
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-09-16 12:40:33 -0700

    finish Part I front-end auth

--- w7d5/client/components/App.jsx
+++ w7d5/client/components/App.jsx
@@ -4,6 +4,8 @@ import { Route, Link } from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
 import SessionFormContainer from './session_form/session_form_container';
 
+import { AuthRoute } from '../util/route_util';
+
 const App = () => (
   <div>
     <header>
@@ -14,8 +16,8 @@ const App = () => (
       <GreetingContainer />
     </header>
 
-    <Route path="/login" component={SessionFormContainer} />
-    <Route path="/signup" component={SessionFormContainer} />
+    <AuthRoute path="/login" component={SessionFormContainer} />
+    <AuthRoute path="/signup" component={SessionFormContainer} />
   </div>
 );
 
