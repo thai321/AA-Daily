b2fbcfe742f86a15dc8f266e3e02f377c346e3cb
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-10-17 20:27:15 -0700

    use hashmap instead of built hash

--- Algorithms/2/quocthai_nguyentruong/lib/p07_practical.rb
+++ Algorithms/2/quocthai_nguyentruong/lib/p07_practical.rb
@@ -1,10 +1,15 @@
 require_relative 'p05_hash_map'
 
 def can_string_be_palindrome?(string)
-  hash = Hash.new(0)
+  # hash = Hash.new(0)
+  hash = HashMap.new
 
   string.each_char do |char|
-    hash[char] += 1
+    if hash[char]
+      hash[char] += 1
+    else
+      hash[char] = 1
+    end
   end
 
   odds = 0
